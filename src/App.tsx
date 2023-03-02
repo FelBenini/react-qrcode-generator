1e6060cb5d3d68627147a1a86584f9276f8fdeba
FelBenini <felbenini@gmail.com>
2023-03-02 14:49:44 -0300

    Changed css responsivity and style

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import './style/App.css';
 import Header from './components/header';
 import Form from './components/form';
 import DownloadButton from './components/downloadButton';
+import { BiQr } from 'react-icons/bi'
 
 function App() {
   const [url, setUrl] = useState('')
@@ -16,7 +17,9 @@ function App() {
         <Form setUrl={setUrl} color={color} setColor={setColor} scale={scale} setScale={setScale}/>
         <div id='QrContainer'>
           <h4>Your QR Code will appear here</h4>
-          {url !== '' ? <QRCode id='QRCode' value={url} size={scale} fgColor={color[0]} bgColor={color[1]} />: <span></span>}
+          {url !== '' ? 
+          <QRCode id='QRCode' value={url} size={scale} fgColor={color[0]} bgColor={color[1]} />: 
+          <span id='blankQR'><BiQr size={'100%'} color={'B2B9BB'}/></span>}
           <DownloadButton/>
         </div>
       </section>
