4633a2227c85d5b3125bbdc9e8291a3bd7a87cf2
FelBenini <felbenini@gmail.com>
2023-03-01 21:53:55 -0300

    Changes made in the UI

--- src/App.tsx
+++ src/App.tsx
@@ -7,12 +7,18 @@ import Form from './components/form';
 
 function App() {
   const [url, setUrl] = useState('')
+  const [color, setColor] = useState(['#FFFFFF', '#000000'])
   return (
     <div className="App">
-      <Header/>
-      <Form setUrl={setUrl}/>
-      <QRCode id='QRCode' value={url} size={256} fgColor='#FFFFFF' bgColor='#000000'/>
-      <button onClick={qrDownloader}>Download QRCode</button>
+      <Header />
+      <section>
+        <Form setUrl={setUrl} color={color} setColor={setColor}/>
+        <div>
+          <h4>Your QR Code will appear here</h4>
+          <QRCode id='QRCode' value={url} size={256} fgColor={color[0]} bgColor={color[1]} />
+          <button onClick={qrDownloader}>Download QRCode</button>
+        </div>
+      </section>
     </div>
   );
 }
--- src/components/form.tsx
+++ src/components/form.tsx
@@ -1,16 +1,22 @@
 import React from 'react'
 
 interface propsTypes {
-    setUrl: React.Dispatch<React.SetStateAction<string>>
+    setUrl: React.Dispatch<React.SetStateAction<string>>,
+    color: Array<string>,
+    setColor: React.Dispatch<React.SetStateAction<Array<string>>>,
 }
 
 
-const Form = ({setUrl}: propsTypes) => {
-  return (
-    <form>
-        <input type="text" onChange={(e) => {setUrl(e.target.value)}}/>
-    </form>
-  )
+const Form = ({ setUrl, color, setColor }: propsTypes) => {
+    return (
+        <form>
+            <input type="text" onChange={(e) => { setUrl(e.target.value) }} />
+            <span className='colorWrapper'>
+                <input type="color" className='colorInput' value={color[0]} onChange={(e) => { setColor([e.target.value, color[1]]) }} />
+                <input type="color" className='colorInput' value={color[1]} onChange={(e) => { setColor([color[0], e.target.value]) }} />
+            </span>
+        </form>
+    )
 }
 
 export default Form
