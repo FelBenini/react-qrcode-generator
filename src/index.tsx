a0220bb69c4b716f45f64b1ba405026eb2e69378
FelBenini <felbenini@gmail.com>
2023-03-02 18:31:16 -0300

    Added custom color and font to material UI components

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,26 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './style/index.css';
 import App from './App';
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ['"Voces"', 'Open Sans'].join(',')
+   },
+  palette: {
+    primary: {
+      main: "#07698a"
+    }
+  }
+})
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={theme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
